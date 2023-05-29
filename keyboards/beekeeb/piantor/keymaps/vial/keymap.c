ae1492a4c1f83dc20d63ccbab36d53b3e23bd7b9
Viorel Stirbu <viorels@gmail.com>
2023-05-29 11:14:25 +0300

    minus not affected by caps word

--- keyboards/beekeeb/piantor/keymaps/vial/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/vial/keymap.c
@@ -197,7 +197,6 @@ bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Keycodes that continue Caps Word, with shift applied.
         case KC_A ... KC_Z:
-        case KC_MINS:
             add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
             return true;
 
@@ -205,6 +204,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_1 ... KC_0:
         case KC_BSPC:
         case KC_DEL:
+        case KC_MINS:
         case KC_UNDS:
         case KC_TAB:  // extra key that does not break CAPS_WORD
             return true;
